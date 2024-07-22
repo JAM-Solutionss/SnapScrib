3a1333e885e6352f36016b2a958abd4e28457b49
mirixy <miriam.lucy@outlook.com>
2024-07-22 14:00:08 +0200

    added comments to app.py

--- SnapScrib/app.py
+++ SnapScrib/app.py
@@ -8,6 +8,9 @@ if __name__ == "__main__":
     url = "https://www.youtube.com/watch?v=iXIwm4mCpuc"
     output= "./youtube_audio/output/audio.mp3"
     filename= "transcription"
+    # downloads the audio from the youtube video
     download_youtube_video_as_audio(url)
+    # checks which operating system is used and runs the appropriate command
+    # then it will trigger the transcription process
     check_os(output, filename)
     
