52ed8ae9e27c3c43e4ed07ed8ecc77707ea13c75
abahde <71208362+arvedb@users.noreply.github.com>
2024-07-28 13:32:28 +0200

    changed the os specific handling of imports in transcribe.py

--- SnapScrib/transcription/transcribe.py
+++ SnapScrib/transcription/transcribe.py
@@ -1,12 +1,14 @@
-import platform
+import sys
+from .transcribe_whisper import transcribe_audio as whisper
+
+if sys.platform == 'darwin':
+    from .transcribe_mlx import transcribe as mlx
+    from .transcribe_with_lightning_mlx import transcribe as lightning
 
 
 def check_os(path, filename):
-    from .transcribe_mlx import transcribe as mlx
-    from .transcribe_with_lightning_mlx import transcribe as lightning
-    from .transcribe_whisper import transcribe_audio as whisper
-    system = platform.system()
-    if system == "Darwin":
+    system = sys.platform
+    if system == "darwin":
         print("This system is running macOS.")
         choose = input("Choose 1: mlx or 2: lightning mlx: ")
         if choose == "1":
@@ -15,11 +17,11 @@ def check_os(path, filename):
             lightning(path, filename)
 
             
-    elif system == "Windows":
+    elif system == "win32":
         print("This system is running Windows.")
         whisper(path, filename)
 
-    elif system == "Linux":
+    elif system == "linux":
         print("This system is running Linux.")
         whisper(path, filename)
 
