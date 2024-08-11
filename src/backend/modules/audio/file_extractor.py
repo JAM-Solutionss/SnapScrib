7142fde999869eb0588402534cfba0cb1815814f
abahde <71208362+arvedb@users.noreply.github.com>
2024-08-11 09:15:28 +0200

    basic implementation of FileExtractor

---
+++ src/backend/modules/audio/file_extractor.py
@@ -0,0 +1,7 @@
+from audio_extractor import AudioExtractor
+
+class FileExtractor(AudioExtractor):
+    
+    def extract(self, file_source: str) -> str:
+        '''Extracting audio from a file'''
+        pass
