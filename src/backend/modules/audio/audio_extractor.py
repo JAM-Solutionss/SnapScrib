d30f4a03f27e29cffe08fcfeb65ebcb0a94b47ca
abahde <71208362+arvedb@users.noreply.github.com>
2024-08-11 09:08:04 +0200

    added docstring

--- src/backend/modules/audio/audio_extractor.py
+++ src/backend/modules/audio/audio_extractor.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
 class AudioExtractor(ABC):
-    
+    '''Abstract interface class for AudioExtractor'''
     @abstractmethod
     def extract(self, audio_source: str) -> str:
         '''This method needs to be implemmented by the child class'''
