5265bdd6faf1da53101f53c0b6fcbcc5bcae3729
abahde <71208362+arvedb@users.noreply.github.com>
2024-08-11 10:35:43 +0200

    implemented DummyAudio DataClass and FileExtractor

--- src/backend/modules/audio/audio_extractor.py
+++ src/backend/modules/audio/audio_extractor.py
@@ -1,8 +1,17 @@
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Type
+
+@dataclass
+class DummyAudio():
+    '''Dummy Adio dataclass for testing and implementing AudioExtractors. Will be replaced by Audio dataclass later on.'''
+    audio_file: str
+
 
 class AudioExtractor(ABC):
     '''Abstract interface class for AudioExtractor'''
+    
     @abstractmethod
-    def extract(self, audio_source: str) -> str:
+    def extract(self, audio_source: str) -> Type[DummyAudio]:
         '''This method needs to be implemmented by the child class'''
         pass
--- src/backend/modules/audio/file_extractor.py
+++ src/backend/modules/audio/file_extractor.py
@@ -1,7 +1,26 @@
-from audio_extractor import AudioExtractor
+from audio_extractor import AudioExtractor, DummyAudio
+from typing import Type
+import os
+
 
 class FileExtractor(AudioExtractor):
-    
-    def extract(self, file_source: str) -> str:
-        '''Extracting audio from a file'''
-        pass
+      def extract(self, file_source: str) -> Type[DummyAudio]:
+          """
+          Extract audio from a file source.
+
+          Args:
+              file_source (str): The path to the audio file.
+
+          Returns:
+              str: A Audio instance representing the extracted audio.
+
+          Raises:
+              ValueError: If the provided file path is invalid.
+          """
+
+          if os.path.isfile(file_source):
+              # file_source is a valid file path
+              return DummyAudio(audio_file=file_source) # Instantiation needs to be adjusted later, when Adio dataclass is implemented
+          else:
+              # file_source is not a valid file path
+              raise ValueError(f"Invalid file path: {file_source}")
