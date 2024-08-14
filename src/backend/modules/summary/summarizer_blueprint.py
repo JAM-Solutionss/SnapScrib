5375672b5dc526ca5390907b997e471d2ff124bf
Cipher Shadow <mprogrammierer@gmail.com>
2024-08-14 12:10:29 +0200

    abstract method

--- src/backend/modules/summary/summarizer_blueprint.py
+++ src/backend/modules/summary/summarizer_blueprint.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
-
+from typing import Type
+from transcription import Transcription
 
 class Summarizer(ABC):
     @abstractmethod
-    def summarize(text):
+    def summarize(transcription: Type[Transcription]) -> str:
         pass
