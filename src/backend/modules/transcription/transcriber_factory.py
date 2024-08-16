6dbb10f5ddeb18a0a75528441355550ac792d8bf
abahde <71208362+arvedb@users.noreply.github.com>
2024-08-16 20:22:30 +0200

    implemented get_transcriber factory method

--- src/backend/modules/transcription/transcriber_factory.py
+++ src/backend/modules/transcription/transcriber_factory.py
@@ -1,33 +1,58 @@
 import sys
+from src.backend.modules.transcription.lightning_mlx_transcriber import (
+    LightningMlxTranscriber,
+)
+from src.backend.modules.transcription.mlx_transcriber import MlxTranscriber
 from src.backend.modules.transcription.trranscriber_interface import Transcriber
+from src.backend.modules.transcription.whisper_transcriber import WhisperTranscriber
 
+# System-specific transcriber classes
 transcribers = {
     "whisper": {
-        'supported_operating_systems': []
-    }
+        "supported_operating_systems": ["win32", "linux"],
+        "transcriber_class": WhisperTranscriber,
+    },
+    "mlx": {
+        "supported_operating_systems": ["darwin"],
+        "transcriber_class": MlxTranscriber,
+    },
+    "lightning_mlx": {
+        "supported_operating_systems": ["darwin"],
+        "transcriber_class": LightningMlxTranscriber,
+    },
 }
 
-def get_transcriber(type: str= None) -> Transcriber:
+
+def get_transcriber(type: str = None) -> Transcriber:
+    """
+    Get the appropriate transcriber instance based on the specified type and the current operating system.
+
+    Args:
+        type (str, optional): The type of transcriber to get. If not provided, the first supported transcriber will be returned.
+
+    Returns:
+        Transcriber: The transcriber class.
+
+    Raises:
+        ValueError: If the specified transcriber type is not supported on the current operating system, or if none transcriber is supported on the current operating system.
+    """
+
     operating_system = sys.platform
-    if operating_system == "darwin":
-        LOGGER.info("This system is running macOS.")
-        LOGGER.debug("Choosing transcription method...")
-        choose = input("Choose 1: mlx or 2: lightning mlx: 3: YT transcription")
-        if choose == "1":
-            return MLX_Transcriber
-        elif choose == "2":
-            return Lightning_MLX_Transcriber
-        else:
-            return YT_Transcriber
 
-    elif operating_system == "win32":
-        LOGGER.info("This system is running Windows.")
-        return Whisper_Transcriber
-
-    elif operating_system == "linux":
-        LOGGER.info("This system is running Linux.")
-        return Whisper_Transcriber
-
+    if type.lower() is None:
+        for name, transcriber_info in transcribers.items():
+            if operating_system in transcriber_info["supported_operating_systems"]:
+                return transcriber_info["transcriber_class"]
+        raise ValueError(
+            f"No transcriber is supported on this operating system: {operating_system}"
+        )
     else:
-        LOGGER.error("Unknown operating system.")
-        return None    
+        if type.lower() in transcribers:
+            if operating_system in transcribers[type]["supported_operating_systems"]:
+                return transcribers[type]["transcriber_class"]
+            else:
+                raise ValueError(
+                    f"Transcriber '{type}' is not supported on this operating system."
+                )
+        else:
+            raise ValueError(f"Unknown transcriber type: {type}")
