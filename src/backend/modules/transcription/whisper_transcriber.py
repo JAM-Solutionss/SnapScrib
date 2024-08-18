11b64633031bf414e26be464ab7536a233fe24fb
abahde <71208362+arvedb@users.noreply.github.com>
2024-08-18 19:25:13 +0200

    started implementing whisper_transcriber

--- src/backend/modules/transcription/whisper_transcriber.py
+++ src/backend/modules/transcription/whisper_transcriber.py
@@ -16,12 +16,22 @@ from transcriber_interface import Transcriber
 
 
 class WhisperTranscriber(Transcriber):
+    
+    default_model = "base"
+    available_models = ["tiny", "base", "small", "medium", "large"]
+    has_cuda = torch.cuda.is_available()
+    device = "cuda" if has_cuda else "cpu"
+    
+    def __init__(self, model: str=default_model) -> None:
+        self.model = whisper.load_model("base")
+
     def transcribe(self, audio: Audio) -> Transcription:
         pass
 
 
 # class Whisper_Transcriber(Transcriber):
-    def transcribe(audio_file: str, youtube_url: str) -> str:
+    def transcribe(self, audio: Audio) -> Transcription:
+      
         torch.cuda.init()
         device = "cuda"
 
@@ -50,3 +60,34 @@ class WhisperTranscriber(Transcriber):
             result = "".join(text)
 
         return str(result)
+    
+    
+    # def transcribe(audio_file: str, youtube_url: str) -> str:
+    #     torch.cuda.init()
+    #     device = "cuda"
+
+    #     model = whisper.load_model("base").to(device=device)
+    #     LOGGER.info("Whisper model loaded.")
+    #     with torch.cuda.device(device):
+    #         transcribe = model.transcribe(audio=audio_file)
+    #     segments = transcribe["segments"]
+
+    #     json_output = []
+    #     for segment in segments:
+    #         startTime = str(0) + str(timedelta(seconds=int(segment["start"]))) + ",000"
+    #         endTime = str(0) + str(timedelta(seconds=int(segment["end"]))) + ",000"
+    #         text = segment["text"]
+    #         segmentId = segment["id"] + 1
+    #         segment = f"{segmentId}\n{startTime} --> {endTime}\n{text[1:] if text[0] == ' ' else text}\n\n"
+
+    #         segment_dict = {
+    #             "id": segmentId,
+    #             "start_time": startTime,
+    #             "end_time": endTime,
+    #             "text": text[1:] if text[0] == " " else text,
+    #         }
+    #         json_output.append(segment_dict)
+
+    #         result = "".join(text)
+
+    #     return str(result)
