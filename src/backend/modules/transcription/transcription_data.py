5406b033f31eef865db697c76b12ada3bfcfe2e2
abahde <71208362+arvedb@users.noreply.github.com>
2024-08-18 09:39:02 +0200

    Reworked Transcrition data class

--- src/backend/modules/transcription/transcription_data.py
+++ src/backend/modules/transcription/transcription_data.py
@@ -2,4 +2,12 @@ from dataclasses import dataclass
 
 @dataclass
 class Transcription:
+    """
+    Transcription dataclass, that holds the transcription data.
+    
+    Attributes:
+        text: str - Transcription text
+        json_output: dict - JSON output from transcription model in the format of (timestamps in seconds): [{"text": "Hey there", "start": 7.58, "end": 8.87, "duration": 1.29}, ...]
+    """
+    text: str
     json_output: dict
