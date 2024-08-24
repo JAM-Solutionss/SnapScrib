6bf59223fa960df78e68e233a9f5f658113c1130
abahde <71208362+arvedb@users.noreply.github.com>
2024-08-24 22:07:41 +0200

    implemented AppData

---
+++ src/backend/modules/app/app_data.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+import sys
+import os
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from audio.audio_data import Audio
+from audio.audio_extractor_interface import AudioExtractor
+from transcription.transcriber_interface import Transcriber
+from transcription.transcription_data import Transcription
+
+@dataclass
+class AppData:
+    audio_extractor: AudioExtractor = None
+    audio: Audio = None
+    transcriber: Transcriber = None
+    transcription: Transcription = None
