d97b75b463489be155cd07b03f9571a7c40433ab
abahde <71208362+arvedb@users.noreply.github.com>
2024-08-24 17:54:40 +0200

    AudioExtractor documenttion

--- src/backend/modules/audio/audio_extractor_interface.py
+++ src/backend/modules/audio/audio_extractor_interface.py
@@ -7,7 +7,21 @@ from audio.audio_data import Audio
 
 
 class AudioExtractor(ABC):
-    """Abstract interface class for AudioExtractor"""
+    
+    """
+    Abstract base class for audio extraction.
+
+    This class defines the interface for audio extractors. Subclasses should
+    implement the `extract` method to provide specific extraction functionality.
+
+    Attributes:
+        supported_audio_files (list): A list of supported audio file extensions.
+        _default_audio_file_save_path (str): The default path for saving extracted audio files.
+
+    Methods:
+        __init__(self, audio_file_path=None): Initialize the AudioExtractor.
+        extract(self, audio_source: str) -> Audio: Abstract method for audio extraction.
+    """
 
     supported_audio_files = [".mp3", ".wav", ".flac", ".ogg", ".m4a"]
     _default_audio_file_save_path = os.path.join(os.path.dirname(__file__), "download")
