5c49fb3d58f0e97f955e68b794d742094c8ee3bc
abahde <71208362+arvedb@users.noreply.github.com>
2024-09-16 20:09:43 +0200

    minor clean up

--- src/backend/config/__init__.py
+++
@@ -1,5 +0,0 @@
-from .css import load_css
-
-__all__ = [
-    'load_css'
-]
--- src/backend/config/css.py
+++
@@ -1,3 +0,0 @@
-def load_css(file_path: str) -> str:
-    with open(file_path, 'r') as file:
-        return f'<style>{file.read()}</style>'
